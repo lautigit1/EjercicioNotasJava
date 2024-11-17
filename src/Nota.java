863c8b6ca4084faa41d6a17b3fe27268ffe8499a
lautigit1 <salinaslautii@gmail.com>
2024-11-17 19:38:04 -0300

    git remote add origin https://github.com/lautigit1/EjercicioNotasJava.git git branch -M main git push -u origin main

---
+++ src/Nota.java
@@ -0,0 +1,18 @@
+public class Nota {
+    private String catedra;
+    private double notaExamen;
+
+    public Nota(String catedra, double notaExamen) {
+        this.catedra = catedra;
+        this.notaExamen = notaExamen;
+    }
+
+    public double getNotaExamen() {
+        return notaExamen;
+    }
+
+    @Override
+    public String toString() {
+        return "Cátedra: " + catedra + ", Nota: " + notaExamen;
+    }
+}
